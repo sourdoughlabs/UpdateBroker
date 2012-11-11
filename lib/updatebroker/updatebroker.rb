1c7a81b91f9097c136d192cb7a13e6c632ad42c5
Vince Hodges <vhodges@gmail.com>
2012-11-11 07:24:59 -0800

    Updated to use a single redis connection for all channels and connect clients.
    For some reason it was still refusing to close redis connections under some unknown conditions
    so I've updated it to use a single connection to redis.

--- lib/updatebroker/updatebroker.rb
+++ lib/updatebroker/updatebroker.rb
@@ -1,5 +1,5 @@
 #
-# (c) 2011 Sourdough Labs Research and Development Corp
+# (c) 2011, 2012 Sourdough Labs Research and Development Corp
 #
 
 require 'rubygems'
@@ -12,25 +12,50 @@ class UpdateBroker < Goliath::API
 
   attr_accessor :redis
 
+  def initialize(opts = {})
+    @channels = {}
+  end
+
+  def add_channel_handler(channel, handler)
+    handlers = @channels[channel] || []
+    handlers << handler
+    @channels[channel] = handlers
+  end
+
+  # Don't call unless add_channel_stream has been called at least once for the given channel
+  def remove_channel_handler(channel, handler)
+    handlers = @channels[channel]
+    handlers.delete(handler) unless handlers.nil?
+  end
+
+  def send_message_to_handlers(channel, msg)   
+    handlers = @channels[channel]
+    unless handlers.nil?
+      handlers.each do |e|
+        res = e.stream_send("data:#{msg}\n\n")
+      end
+    end
+  end
+
   def response(env)
     path = env[Goliath::Request::REQUEST_PATH]
-    env.logger.info "Request for '#{path}'"
     return [404, {}, "Not found"] unless path == "/events"
 
     channel = env.params[:channel] || env.params['channel']
-    env.logger.info "On channel '#{channel}'"
     return [404, {}, "Channel required"] if channel.nil? || channel == ""
 
-    env.logger.info "Connecting to channel '#{channel}'@#{options[:redis]} for updates"
-
-    env['redis'] = EM::Hiredis.connect(options[:redis])
-    env['redis'].subscribe(channel)
-    env['redis'].on(:message) do |chn, msg|
-      env.logger.info "Message received from channel #{chn}: #{msg} (We're looking for #{channel})"
-      if chn === channel
-        res = env.stream_send("data:#{msg}\n\n")
+    if self.redis.nil?
+      env.logger.info "Redis connection is down, Connecting to @#{options[:redis]}"
+      self.redis = EM::Hiredis.connect(options[:redis]) 
+      redis.on(:message) do |chn, msg|
+        env.logger.info "Message for channel '#{chn}':"        
+        send_message_to_handlers(chn,msg) 
       end
     end
+
+    env.logger.info "subscribing to channel '#{channel}' for updates"
+    redis.subscribe(channel)
+    add_channel_handler(channel, env)
 
     streaming_response(200, {'Content-Type' => 'text/event-stream'})
   end
@@ -43,11 +68,8 @@ class UpdateBroker < Goliath::API
     return if channel.nil? || channel == ""
 
     env.logger.info "Connection closed, Unsubscribing."
-    unless env['redis'].nil?
-      env['redis'].unsubscribe(channel)
-      env['redis'].quit
-      env['redis'].close_connection
-    end
+    redis.unsubscribe(channel)
+    remove_channel_handler(channel, env)
   end
 
   def options_parser(opts, options)
